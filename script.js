bbe99ccc1645708d6de995515d80e0d0f9576d10
Luiz Gustavo M. Lopes <luiz.gustavo.machado@hotmail.com>
2022-09-20 19:22:05 -0300

    new page JS

---
+++ script.js
@@ -0,0 +1,9 @@
+new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    loop: true,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, 
+    },
+    });
