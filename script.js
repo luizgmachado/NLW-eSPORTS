7391699e66170d18093b9cce3759c7729295601e
luizgmachado <luiz.gustavo.machado@hotmail.com>
2023-03-21 20:09:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ new Swiper(".m1Swiper", {
     slidesPerView: 3,
     loop: true,
     spaceBetween: 10,
-    /*pagination: {
-      el: ".swiper-pagination1",
-      clickable: true, 
-    },*/
+
     });
 
 
@@ -13,8 +10,4 @@ new Swiper(".m1Swiper", {
       slidesPerView: 4,
       loop: true,
       spaceBetween: 10,
-      /*pagination: {
-        el: ".swiper-pagination",
-        clickable: true, 
-      },*/
       });
